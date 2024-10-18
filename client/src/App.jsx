54d9eae30a66b3b13377112c2e3e799a3235ef61
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-17 23:58:36 -0500

    tests connection from express server to react client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import './App.css'
 
-function App() {
- 
+const App = () => {
+  
+  // tests connection from express server to react client
+  // creates initial state for message
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3001/')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
 
   return (
-    <>
-    <h1>My Crochet Corner</h1>
-    </>
+    <div>App
+      <p>{message}</p>
+    </div>
   )
 }
 
-export default App
+export default App;
