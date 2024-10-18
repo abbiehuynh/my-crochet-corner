5964b746b4efebc656e1d294ca6672de7ab7a943
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-18 13:33:30 -0500

    adds search and sort components to favorites page

--- client/src/components/favorites-page/Favorites.jsx
+++ client/src/components/favorites-page/Favorites.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import SearchBar from '../home/SearchBar';
+import SortProjectType from './SortProjectType';
 import ListFavoriteProjects from './ListFavoriteProjects';
 
 const Favorites = () => {
   return (
     <div>Favorites
-
+        <SearchBar />
+        <SortProjectType />
         <ListFavoriteProjects />
 
         <Link to={`/add-project`}>
