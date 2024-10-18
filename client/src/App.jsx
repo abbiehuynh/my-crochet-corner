83e3f8bfb469bd30db6f75a89555d31e8832d4e5
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-18 00:43:59 -0500

    sets up basic routing in NavBar to main pages - home, favorites, and profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/home/Home';
+import Favorites from './components/favorites-page/Favorites';
+import Profile from './components/profile-page/Profile';
 import './App.css'
 
 const App = () => {
   
   // tests connection from express server to react client
   // creates initial state for message
+  // documenting test connection, will remove after PR
   const [message, setMessage] = useState('');
 
   useEffect(() => {
@@ -17,7 +23,20 @@ const App = () => {
 
   return (
     <div>App
+      {/* test connection, will remove after documented in PR */}
       <p>{message}</p>
+
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/profile" element={<Profile />} />
+
+        </Routes>
+      </Router>
+
+
     </div>
   )
 }
