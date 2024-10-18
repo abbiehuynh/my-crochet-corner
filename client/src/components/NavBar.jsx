5d863de7783e55afb951a46e4cbe799d086833a3
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-18 00:43:59 -0500

    sets up basic routing in NavBar to main pages - home, favorites, and profile page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav>
+        <h1>NavBar</h1>
+        <Link to="/">Home</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/profile">Profile</Link>
+    </nav>
+
   )
 }
 
