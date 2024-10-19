1edbbdf45f4f2f6764f465d112afcf0f7546f0e1
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-18 20:37:16 -0500

    add button components to sort by project type for favorites page

--- client/src/components/favorites-page/SortProjectType.jsx
+++ client/src/components/favorites-page/SortProjectType.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const SortProjectType = () => {
   return (
-    <div>SortProjectType</div>
+    <div>
+        {/* will allow users to sort through and view projects by project type */}
+        SortProjectType
+
+        <Button >All Projects</Button>
+
+        <Button>Amigurimi</Button>
+        <Button>Blanket</Button>
+        <Button>Clothing</Button>
+        <Button>Tapestry</Button>
+
+        {/* will allow users to filter projects by alphabetical order and date (most recent - latest) */}
+        <Button >Sort By</Button>
+    </div>
   )
 }
 
