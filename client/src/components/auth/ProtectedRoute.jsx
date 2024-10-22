2a0314efe8cfaa6b91ec2e80cd72c81cf41039e2
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-22 14:37:49 -0500

    adds protectedRoute component to prevent users from accessing app until they have logged in, also remove uneccessary testing code in app

---
+++ client/src/components/auth/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+ 
+const ProtectedRoute = ({ element }) => {
+    const token = localStorage.getItem('token');
+
+    return token ? element : <Navigate to="/login" />;
+
+};
+
+export default ProtectedRoute;
