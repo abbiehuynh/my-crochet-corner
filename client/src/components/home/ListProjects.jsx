3b501e7fc66038263d7366d4805fc4c59b3ee4ad
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-21 20:43:49 -0500

    creates get request to projects table to create a list of projects

--- client/src/components/home/ListProjects.jsx
+++ client/src/components/home/ListProjects.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import ProjectCard from './ProjectCard';
 
-
+// update to pass userId as prop
 const ListProjects = () => {
-  return (
-    <div>
-        {/* this will show a list of all projects */}
-        ListProjects
 
-        {/* project card will be mapped to display each project as a list */}
-        <ProjectCard />
+    // creates initial state of list of projects
+    const [projects, setProjects] = useState([]);
+
+    const loadProjects = () => {
+        // fetches the list of projects
+        // update to fetch by userId
+        // fetch(`http://localhost:3001/user/${userId}/projects`)
+            fetch(`http://localhost:3001/testTable/projects`)
+            .then((response) => response.json())
+            .then((projects) => {
+                setProjects(projects);
+            });
+    }
+
+    useEffect(() => {
+        loadProjects();
+    }, [projects]);
+
+
+
+  return (
+    <div className="container">
+        <div className="box list-projects">
+            {/* TO DO: set to value in database so it can update with sorting button - All Projects */}
+            <h2>List of Projects</h2>
+
+            {/* creates a list of projects by mapping projectCard */}
+            <ul>
+                {projects.map((project) => {
+                    return <li key={project.id}> <ProjectCard project={project} /></li>
+                })}
+            </ul>
+
+
+        </div>
     </div>
   )
 }
