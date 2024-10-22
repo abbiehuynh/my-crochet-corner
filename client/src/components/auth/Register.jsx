861b7f68617e0fc9c69b2349acdf41da6bd65991
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-22 12:31:48 -0500

    redirects user from register to login after successfully creating account

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
-import { Form, Button, Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { Form, Button, Container, Alert } from 'react-bootstrap';
 
 const Register = () => {
     // creates initial states
@@ -9,32 +8,45 @@ const Register = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    // allows to use useNavigate to redirect to link
+    const navigate = useNavigate();
 
     const handleRegister = async (e) => {
         e.preventDefault();
         try {
-          const response = await fetch('http://localhost:3001/register', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ name, username, email, password }), // Include email in the request
-          });
+            const response = await fetch('http://localhost:3001/register', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ name, username, email, password }), // Include email in the request
+            });
 
-          const data = await response.json();
-      if (response.ok) {
-        console.log('User registered:', data);
-      } else {
-        console.error('Registration error:', data.error);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
+            const data = await response.json();
+            if (response.ok) {
+                setMessage('Your account has been created! Redirecting to login...')
+                console.log('User registered:', data);
+                setTimeout(() => {
+                    navigate('/login');
+                // wait for 2 seconds before executing
+                }, 2000);
+
+            } else {
+                setMessage(data.message || 'Registration failed. Please try again.')
+                console.error('Registration error:', data.error);
+            }
+
+        } catch (error) {
+            setMessage("An error occured. Please try again later.")
+            console.error('Error:', error);
+        }
+    };
 
   return (
     <Container>
       <h2>Register</h2>
+      {message && <Alert variant="success">{message}</Alert>}
       <Form onSubmit={handleRegister}>
         <Form.Group controlId="formName"> {/* New name input */}
             <Form.Label>Name</Form.Label>
