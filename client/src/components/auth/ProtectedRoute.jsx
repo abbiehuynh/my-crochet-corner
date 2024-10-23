741ea6d1df42dda45843f56d862b07ebc398c658
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-23 01:22:09 -0500

    adds useAuth to protectedRoute component

--- client/src/components/auth/ProtectedRoute.jsx
+++ client/src/components/auth/ProtectedRoute.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { useAuth } from './AuthProvider';
  
 const ProtectedRoute = ({ element }) => {
-    const namespacedTokenKey = 'MCC_Token';
-    const token = localStorage.getItem(namespacedTokenKey);
+    const { token } = useAuth();
 
     // if the token doesn't exist, redirect to the login page
     if (!token) {
         return <Navigate to="/login" />;
     }
 
-    // if the tokoen exists, render the request component
+    // if the token exists, render the request component
     return element;
-
 };
 
 
