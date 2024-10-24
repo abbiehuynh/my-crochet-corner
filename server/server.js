0e56b0a8a62a9b20b1fa17cb6c0c84edc225a106
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-24 17:30:20 -0500

    updates paths to build file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const { fileURLToPath } = require('url');
+const path = require('path');
 require('dotenv').config();
 
 // imports the database connection
@@ -14,6 +15,18 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+
+// getting directory name
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// serve static files from react app
+app.use(express.static(path.join(__dirname, '../client/dist' )));
+
+// catch all handler for any request that does not match above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 // TODO: remove test later
 // test - creates an endpoint for the route  "/"
