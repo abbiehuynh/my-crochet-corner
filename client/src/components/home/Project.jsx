4947118f20b0555e7bc0601a9bf646b2b719e74e
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-26 15:18:00 -0500

    saving progress for creating project details page

--- client/src/components/home/Project.jsx
+++ client/src/components/home/Project.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useAuth } from '../auth/AuthProvider';
 import ProjectForm from './ProjectForm';
 
 const Project = () => {
+    const { userId, axiosInstance } = useAuth();
+    const { projectId } = useParams();
+    const [project, setProject] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchProject = async () => {
+            try {
+                const response = await axiosInstance.get(`/user/${userId}/project/${projectId}`);
+                setProject(response.data);
+            } catch (error) {
+                setError('Failed to fetch project details');
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchProject();
+    }, [userId, projectId, axiosInstance]);
+
+    if (loading)
+        return <p>Loading project details...</p>
+
+    if (error)
+        return <p>{error}</p>
+
+
   return (
     <div>
-        {/* this page will display all project details */}
+        <h2>{project.project_name}</h2>
         {/* this will include the project form to allow the user to update project details */}
         Project
         <ProjectForm />
--- client/src/components/home/ProjectCard.jsx
+++ client/src/components/home/ProjectCard.jsx
@@ -3,8 +3,8 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button'
 import { Link } from 'react-router-dom';
 
-const ProjectCard = ({ project, onDelete }) => {
-    
+const ProjectCard = ({ project, userId, onDelete }) => {
+
   return (
     <Card>
         <Card.Body>
@@ -17,7 +17,7 @@ const ProjectCard = ({ project, onDelete }) => {
             {/* the user can click the button to view all of the project details */}
             {/* should be updated to /user/${userId}/project/${projectId} */}
             <Button>
-                <Link to={`/user/project`} style={{ color: "white", textDecoration: "none" }}>Open Project</Link>
+                <Link to={`/user/${userId}/project/${project.id}`} style={{ color: "white", textDecoration: "none" }}>Open Project</Link>
             </Button> 
             <Button variant="danger" onClick={onDelete}>Delete Project</Button>
         </Card.Body>
