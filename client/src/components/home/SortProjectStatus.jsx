0623e15d8266f92d8cb256373c78211f79937e7e
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-29 15:19:11 -0500

    creates UI buttons for sort and filter categories; accesses functions via project context

--- client/src/components/home/SortProjectStatus.jsx
+++ client/src/components/home/SortProjectStatus.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import { Button, ButtonGroup } from 'react-bootstrap';
+import { useProjects } from './ProjectProvider';
 
 const SortProjectStatus = () => {
+    const { setSortOrder, setSelectedCategory } = useProjects();
+
   return (
-    <div>SortProjectStatus
-        {/* will allow users to sort through projects by project status */}
+    <div className="mb-3">
+        <h3>Sort By:</h3>
+        <ButtonGroup>
+            <Button variant="secondary" onClick={() => setSortOrder('name')}>Name</Button>
+            <Button variant="secondary" onClick={() => setSortOrder('type')}>Type</Button>
+            <Button variant="secondary" onClick={() => setSortOrder('date')}>Date</Button>
+        </ButtonGroup>
 
-        <Button >All Projects</Button>
-
-        {/* update to be read from the database */}
-        <Button >In Progress</Button>
-        <Button >To Do</Button>
-        <Button >Completed</Button>
-
-        {/* will allow users to filter projects by alphabetical order and date (most recent - latest) */}
-        <Button >Sort By</Button>
-        
+        <h3>Filter By Project Status:</h3>
+        <ButtonGroup>
+            <Button variant="primary" onClick={() => setSelectedCategory('All')}>All</Button>
+            <Button variant="primary" onClick={() => setSelectedCategory('To Do')}>To Do</Button>
+            <Button variant="primary" onClick={() => setSelectedCategory('In Progress')}>In Progress</Button>
+            <Button variant="primary" onClick={() => setSelectedCategory('Completed')}>Completed</Button>
+        </ButtonGroup>
     </div>
   )
 }
