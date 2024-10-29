08d7cca46fcc16a84632cb2a8f80c907d024c3f0
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-29 15:17:36 -0500

    adds sortProjectStatus component to listProjects component; adds message for -no projects found- if there are no search/sort results

--- client/src/components/home/ListProjects.jsx
+++ client/src/components/home/ListProjects.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { useProjects } from './ProjectProvider';
 import SearchBar from './SearchBar';
+import SortProjectStatus from './SortProjectStatus';
 import ProjectCard from './ProjectCard';
 
 const ListProjects = () => {
     // access from Project Context
-    const { projects, loading, error, deleteProject, setSearchQuery } = useProjects();
+    const { projects, loading, error, deleteProject, setSearchQuery, setSortOrder, setSelectedCategory } = useProjects();
 
     if (loading) 
         return <div>Loading projects...</div>;
@@ -18,14 +19,22 @@ const ListProjects = () => {
         <div className="box list-projects">
             {/* TO DO: set to value in database so it can update with sorting button - All Projects */}
             <h2>List of Projects</h2>
+
             <SearchBar setSearchQuery={setSearchQuery} />
-            <ul style={{ listStyleType: "none" }}>
-                {projects.map((project) => (
-                    <li key={project.id}> 
-                        <ProjectCard project={project} onDelete={() => deleteProject(project.id, project.project_name)} />
-                    </li>
-                ))}
-            </ul>
+            <SortProjectStatus />
+
+            {/* displays message if no projects found */}
+            {projects.length === 0 ? (
+                <p>No projects found.</p>
+            ) : (
+                <ul style={{ listStyleType: "none" }}>
+                    {projects.map((project) => (
+                        <li key={project.id}> 
+                            <ProjectCard project={project} onDelete={() => deleteProject(project.id, project.project_name)} />
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     </div>
   );
