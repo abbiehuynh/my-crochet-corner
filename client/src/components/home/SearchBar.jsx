4e759e4c3b58dbe155e2f31291679090aeac2a0e
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-29 13:52:10 -0500

    creates searchBar UI component; accesses search function via prop from listProjects component

--- client/src/components/home/SearchBar.jsx
+++ client/src/components/home/SearchBar.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Form, InputGroup } from 'react-bootstrap';
 
-const SearchBar = () => {
+const SearchBar = ({ setSearchQuery }) => {
+
   return (
-    <div>SearchBar
-        {/* will allow users to search through the list of projects by project name */}
-
-    </div>
+    <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">Search</InputGroup.Text>
+        <Form.Control
+            type="text"
+            placeholder="Search projects..."
+            onChange={(e) => setSearchQuery(e.target.value)}
+            aria-label="Search"
+            aria-describedby='basic-addon1'
+        />
+    </InputGroup>
   )
 }
 
