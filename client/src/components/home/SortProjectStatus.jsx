5170abec7aa655d9167a14d5da620bfdd9e76473
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-30 11:36:54 -0500

    creates dynamic headers for filter and sort buttons

--- client/src/components/home/SortProjectStatus.jsx
+++ client/src/components/home/SortProjectStatus.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, ButtonGroup } from 'react-bootstrap';
 import { useProjects } from './ProjectProvider';
 
 const SortProjectStatus = () => {
     const { setSortOrder, setSelectedCategory } = useProjects();
+    const [filterHeader, setFilterHeader] = useState('Filter projects by status');
+    const [sortHeader, setSortHeader] = useState('Sort projects by criteria');
+
+    // for project status - to do, in progress, completed
+    const handleFilter = (category, header) => {
+        setSelectedCategory(category);
+        setFilterHeader(header);
+    }
+
+    // for sorting - name, type, date
+    const handleSort = (order, header) => {
+        setSortOrder(order);
+        setSortHeader(header);
+    }
 
   return (
     <div className="mb-3">
-        <h3>Sort By:</h3>
+        <h3>{filterHeader}</h3>
         <ButtonGroup>
-            <Button variant="secondary" onClick={() => setSortOrder('name')}>Name</Button>
-            <Button variant="secondary" onClick={() => setSortOrder('type')}>Type</Button>
-            <Button variant="secondary" onClick={() => setSortOrder('date')}>Date</Button>
+            <Button variant="primary" onClick={() => handleFilter('All', 'All Projects')}>All</Button>
+            <Button variant="primary" onClick={() => handleFilter('To Do', 'To Do')}>To Do</Button>
+            <Button variant="primary" onClick={() => handleFilter('In Progress', 'In Progress')}>In Progress</Button>
+            <Button variant="primary" onClick={() => handleFilter('Completed', 'Completed')}>Completed</Button>
         </ButtonGroup>
 
-        <h3>Filter By Project Status:</h3>
+        <h3>{sortHeader}</h3>
         <ButtonGroup>
-            <Button variant="primary" onClick={() => setSelectedCategory('All')}>All</Button>
-            <Button variant="primary" onClick={() => setSelectedCategory('To Do')}>To Do</Button>
-            <Button variant="primary" onClick={() => setSelectedCategory('In Progress')}>In Progress</Button>
-            <Button variant="primary" onClick={() => setSelectedCategory('Completed')}>Completed</Button>
+            <Button variant="secondary" onClick={() => handleSort('name', 'Name')}>Name</Button>
+            <Button variant="secondary" onClick={() => handleSort('type', 'Type')}>Type</Button>
+            <Button variant="secondary" onClick={() => handleSort('date', 'Date')}>Date</Button>
         </ButtonGroup>
     </div>
   )
