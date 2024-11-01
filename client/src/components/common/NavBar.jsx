d89c7f6dbbadab4c9b3eabd2442747c155eb83e1
abbiehuynh <abbiehuynhh@gmail.com>
2024-11-01 01:43:54 -0500

    uses react bootstrap for navbar components and applys styling via css

--- client/src/components/common/NavBar.jsx
+++ client/src/components/common/NavBar.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import '/Users/tpl622_3/my-crochet-corner/client/src/App.css';
 
 const NavBar = () => {
   return (
-    <nav>
-      {/* creates links to the main pages */}
-        <h1>NavBar</h1>
-        <Link to="/home">Home</Link>
-        <Link to="/favorites">Favorites</Link>
-        <Link to="/profile">Profile</Link>
-    </nav>
-
+    <Navbar style={{ backgroundColor: '#FACDCC', padding: '10px' }} expand="lg">
+      <Navbar.Brand className="navbar-brand" as={Link} to="/home">My Crochet Corner</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link className="navbar-links" as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link className="navbar-links" as={Link} to="/favorites">Favorites</Nav.Link>
+            <Nav.Link className="navbar-links" as={Link} to="/profile">Profile</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
   )
 }
 
