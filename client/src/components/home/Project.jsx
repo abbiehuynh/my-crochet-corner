e39867b5aeca66a7c7c57d62e96ec921a7881eed
abbiehuynh <abbiehuynhh@gmail.com>
2024-11-01 03:13:45 -0500

    adds styling to project details page

--- client/src/components/home/Project.jsx
+++ client/src/components/home/Project.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAuth } from '../auth/AuthProvider';
-import { Container, ListGroup, Alert, Spinner } from 'react-bootstrap';
+import { Button, Card, Container, ListGroup, Alert, Spinner } from 'react-bootstrap';
 import ProjectForm from './ProjectForm';
 
 const Project = () => {
@@ -63,10 +63,10 @@ const Project = () => {
     if (!project) return <p>No project data available.</p>;
 
   return (
-    <Container>
-        <button onClick={handleEditToggle}>
+    <Container style={{ padding: '15px' }}>
+        <Button variant="primary" onClick={handleEditToggle} style={{ marginBottom: '15px', marginLeft: '10px' }}>
             {isEditing ? 'Cancel' : 'Edit Project'}
-        </button>
+        </Button>
         {isEditing ? (
             <ProjectForm 
                 project={project}
@@ -74,68 +74,92 @@ const Project = () => {
                 projectId={projectId}
             />
         ) : (
-            <div>
-                <h2>{project.project_name}</h2>
-                <h3>Project Information:</h3>
-                    <p>Favorite Project: {String(project.is_favorite)}</p>
-                    <p>Project Status: {project.project_status}</p>
-                    <p>Project Type: {project.project_type}</p>
-                    <p>Date Created: {new Date(project.created_at).toLocaleDateString()}</p>
-                    <p>Date Completed: {project.end_at ? new Date(project.end_at).toLocaleDateString() : 'Not completed'}</p>
-                    <p>Time to Complete: {project.time_to_complete?.hours || 'N/A'} hours</p>
-                    <p>Notes: {project.notes || 'N/A'}</p>
+            <>
+                {/* Project Information Card */}
+                <Card className="mb-4">
+                    <Card.Body>
+                        <Card.Title>{project.project_name}</Card.Title>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>Favorite Project: {String(project.is_favorite)}</ListGroup.Item>
+                            <ListGroup.Item>Project Status: {project.project_status}</ListGroup.Item>
+                            <ListGroup.Item>Project Type: {project.project_type}</ListGroup.Item>
+                            <ListGroup.Item>Date Created: {new Date(project.created_at).toLocaleDateString()}</ListGroup.Item>
+                            <ListGroup.Item>Date Completed: {project.end_at ? new Date(project.end_at).toLocaleDateString() : 'Not completed'}</ListGroup.Item>
+                            <ListGroup.Item>Time to Complete: {project.time_to_complete?.hours || 'N/A'} hours</ListGroup.Item>
+                            <ListGroup.Item>Notes: {project.notes || 'N/A'}</ListGroup.Item>
+                        </ListGroup>
+                    </Card.Body>
+                </Card>
 
-                <h3>Patterns:</h3>
-                {project.patterns && project.patterns.length > 0 ? (
-                    <ListGroup>
-                        {project.patterns.map((pattern, index) => (
-                            <ListGroup.Item key={index}>
-                                <strong>Pattern Name:</strong> {pattern.pattern_name} <br />
-                                <strong>Pattern By:</strong> {pattern.pattern_by} <br />
-                                <strong>Pattern Source:</strong> {pattern.pattern_url}
-                            </ListGroup.Item>
-                        ))}
-                    </ListGroup>
-                ) : (
-                    <p>No patterns listed yet.</p>
-                )}
+                {/* Pattern Card */}
+                <Card className='mb-4'>
+                    <Card.Body>
+                        <Card.Title>Patterns:</Card.Title>
+                        {project.patterns && project.patterns.length > 0 ? (
+                                <ListGroup variant="flush">
+                                    {project.patterns.map((pattern, index) => (
+                                        <ListGroup.Item key={index}>
+                                            <strong>Pattern Name:</strong> {pattern.pattern_name} <br />
+                                            <strong>Pattern By:</strong> {pattern.pattern_by} <br />
+                                            <strong>Pattern Source:</strong> {pattern.pattern_url}
+                                        </ListGroup.Item>
+                                    ))}
+                                </ListGroup>
+                            ) : (
+                                <p>No patterns listed yet.</p>
+                            )}
+                    </Card.Body>
+                </Card>
 
-                <h3>Other Materials:</h3>
-                {project.otherMaterials && project.otherMaterials.length > 0 ? (
-                    <ListGroup>
-                        {project.otherMaterials.map((material, index) => (
-                            <ListGroup.Item key={index}>
-                                <strong>Hook Size:</strong> {material.project_hook_size} <br />
-                                <strong>Safety Eyes:</strong> {material.safety_eyes} <br />
-                                <strong>Stuffing:</strong> {material.stuffing}
-                            </ListGroup.Item>
-                        ))}
-                    </ListGroup>
-                ) : (
-                    <p>No other materials listed yet.</p>
-                )}
+                {/* Other Materials Card */}
+                <Card className='mb-4'>
+                    <Card.Body>
+                        <Card.Title>Other Materials:</Card.Title>
+                        {project.otherMaterials && project.otherMaterials.length > 0 ? (
+                            <ListGroup variant="flush">
+                                {project.otherMaterials.map((material, index) => (
+                                    <ListGroup.Item key={index}>
+                                        <strong>Hook Size:</strong> {material.project_hook_size} <br />
+                                        <strong>Safety Eyes:</strong> {material.safety_eyes} <br />
+                                        <strong>Stuffing:</strong> {material.stuffing}
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
+                        ) : (
+                            <p>No other materials listed yet.</p>
+                        )}
+                    </Card.Body>
+                </Card>
 
-                <h3>Yarn</h3>
-                {project.yarns && project.yarns.length > 0 ? (
-                    <ListGroup>
-                        {project.yarns.map((yarn, index) => (
-                            <ListGroup.Item key={index}>
-                                <strong>Brand:</strong> {yarn.yarn_brand} <br />
-                                <strong>Color:</strong> {yarn.yarn_color} <br />
-                                <strong>Weight:</strong> {yarn.yarn_weight} <br />
-                                <strong>Type:</strong> {yarn.yarn_type}
-                            </ListGroup.Item>
-                        ))}
-                    </ListGroup>
-                ) : (
-                    <p>No yarn details available.</p>
-                )}
-            </div>
+                {/* Yarn Card  */}
+                <Card className='mb-4'>
+                    <Card.Body>
+                        <Card.Title>Yarn</Card.Title>
+                        {project.yarns && project.yarns.length > 0 ? (
+                            <ListGroup variant="flush">
+                                {project.yarns.map((yarn, index) => (
+                                    <ListGroup.Item key={index}>
+                                        <strong>Brand:</strong> {yarn.yarn_brand} <br />
+                                        <strong>Color:</strong> {yarn.yarn_color} <br />
+                                        <strong>Weight:</strong> {yarn.yarn_weight} <br />
+                                        <strong>Type:</strong> {yarn.yarn_type}
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
+                        ) : (
+                            <p>No yarn details available.</p>
+                        )}
+                    </Card.Body>
+                </Card>
+
+                <Card>
+                    <Card.Body>
+                        <p>User: {project.user_name}</p>
+                        <p>Updated At: {new Date(project.updated_at).toLocaleString()}</p>
+                    </Card.Body>
+                </Card>
+            </>
         )}
-        <div>
-            <p>User: {project.user_name}</p>
-            <p>Updated At: {new Date(project.updated_at).toLocaleString()}</p>
-        </div>
     </Container>
   );
 }
