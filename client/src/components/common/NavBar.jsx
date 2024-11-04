cf899720985cec350045fbff8ffbbb61a21d9452
abbiehuynh <abbiehuynhh@gmail.com>
2024-11-03 23:23:53 -0600

    updates path to import app.css file in navbar component

--- client/src/components/common/NavBar.jsx
+++ client/src/components/common/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import '/Users/tpl622_3/my-crochet-corner/client/src/App.css';
+import '../../App.css';
 
 const NavBar = () => {
   return (
