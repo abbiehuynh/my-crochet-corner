b1b7b4ac0bf07f6bf40209ab18961910dd70c154
abbiehuynh <abbiehuynhh@gmail.com>
2024-11-05 16:25:53 -0600

    removing setupNodeEvents from config

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:5173'
   },
 });
