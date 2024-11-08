e499f4010969e9e8e2db691da87016a63db7497f
abbiehuynh <abbiehuynhh@gmail.com>
2024-11-08 00:09:44 -0600

    creates logout modal and confirmation when user clicks logout button

--- client/src/components/auth/Logout.jsx
+++ client/src/components/auth/Logout.jsx
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthProvider';
-import Button from 'react-bootstrap/Button';
 
 const Logout = () => {
-    const navigate = useNavigate();
-    // accesses the logout function from use context
-    const { logout } = useAuth();
+  // creates initial state for logout modal
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+  const { logout } = useAuth();
 
-    const handleLogout = () => {
-        logout();
-        navigate('/login');
-    };
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+    setShowModal(false);
+  };
+
+  const handleConfirmLogout = () => {
+    handleLogout();
+    setShowModal(false);
+  }
 
   return (
-    <Button className="logout-btn" onClick={handleLogout} data-test="logout-btn-profile">Logout</Button>
+    <div>
+      {/* logout button */}
+      <Button className="logout-btn" onClick={handleShowModal} data-test="logout-btn-profile">Logout</Button>
+      
+      {/* logout confirmation modal */}
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Are you sure you want to log out?</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>You will be redirected to the login page.</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={handleConfirmLogout}>
+            Logout
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+    </div>
+
+
+
   );
 };
 
